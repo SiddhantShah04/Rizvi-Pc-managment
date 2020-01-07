6e9bafb4c0d3959a7cebc6885a4685aafb8cb4d9
sidd044 <siddhantshah04@gmail.com>
2020-01-07 19:47:18 +0530

    added smis verification

--- Student.java
+++ Student.java
@@ -6,21 +6,22 @@ import javax.swing.*;
 class Student implements ActionListener {
     JTextField jtf;
     JButton jb1;
+    JFrame jfrm;
+    JLabel jlp3;
+    JPanel jp2; 
     Student(){
-        JFrame jfrm = new JFrame();
-       
+        jfrm = new JFrame();
         jfrm.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         jfrm.setUndecorated(true);
         jfrm.setLayout(new GridLayout(3,1));
         //jfrm.setBackground(Color.GREEN);
-
         //Header
         JPanel jp0 = new JPanel(new GridLayout(2,1));
         jfrm.add(jp0);
         JLabel jlab = new JLabel("Rizvi Library Pc's managment system",JLabel.CENTER);
         jlab.setFont(new Font("SansSerif",Font.PLAIN,40));
 
-        JLabel jlab1 = new JLabel("Please enter your details",JLabel.CENTER);
+        JLabel jlab1 = new JLabel("Please enter your detail",JLabel.CENTER);
         jlab1.setFont(new Font("SansSerif",Font.PLAIN,25));
         jp0.add(jlab);        
         jp0.add(jlab1);
@@ -40,29 +41,40 @@ class Student implements ActionListener {
         jb1.addActionListener(this);
         jp1.add(jb1);
         jfrm.add(jp1);
+
+        jp2 = new JPanel(f);
+        jlp3 = new JLabel("Please Enter your correct SMIS number or contact library administration",JLabel.CENTER);
+        jlp3.setFont(new Font("SansSerif",Font.PLAIN,15));
+        jlp3.setForeground(Color.red);
+        jp2.add(jlp3);
+        jp2.setVisible(false);
+        jfrm.add(jp2);
+
+        jfrm.setAlwaysOnTop(true);
         jfrm.setVisible(true);
     }
     public void actionPerformed(ActionEvent e){
-
-        System.out.println(jtf.getText());
+        int i= Integer.parseInt(jtf.getText());
+        System.out.println(i);
         try{
             Class.forName("com.mysql.jdbc.Driver");
             Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/login?characterEncoding=latin1","root","admin");  
-            Statement stmt=con.createStatement(); 
-            ResultSet rs = stmt.executeQuery("select * from student");
-            while(rs.next()){
-                System.out.println(rs.getInt(1));
-                if(rs.getInt(1)){
-                    
-                }
+            PreparedStatement stmt = con.prepareStatement("select * from student where smis=?");
+            stmt.setInt(1,i);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                jp2.setVisible(false);
+                //rest code to write when students write the correct smis number
+                jfrm.setAlwaysOnTop(false);
+            }else{
+                
+                jp2.setVisible(true);
             }
             con.close(); 
         }
         catch(Exception f){
             System.out.println(f);
-
         }
-    
     }
     public static void main(String args[]){
         new Student();
