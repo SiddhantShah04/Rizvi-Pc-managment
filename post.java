aac3b34e9cda4a367196c880d12606c8680e62bd
sidd044 <siddhantshah04@gmail.com>
2020-01-21 15:56:46 +0530

    done

--- post.java
+++ post.java
@@ -5,23 +5,30 @@ import java.time.LocalTime;
 class post{  
 public static void main(String args[]){  
 try{  
+	int t=0;
 	Class.forName("com.mysql.jdbc.Driver");
     //local host is good
     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/login?characterEncoding=latin1","root","admin");  
 	//here sonoo is database name,  is username and password  
 	
-	Statement stmt = con.createStatement();
-    ResultSet rs = stmt.executeQuery("select * from pc");  
-	System.out.println(rs.getInt(1));
-	String sql = "insert into students(endTime) values(?)  where pc=(?)";
+	
+	PreparedStatement stmt=con.prepareStatement("select * from pc");  
+	ResultSet rs=stmt.executeQuery(); 
+	while(rs.next()){  
+		t=rs.getInt(1);  
+	}  
+	System.out.println(t);
+	
+	String sql = "UPDATE students SET endTime=? WHERE pc = ? and endTime is Null";
 	PreparedStatement spst = con.prepareStatement(sql);
 	
 	LocalTime now = LocalTime.now();
 	Time time = Time.valueOf( now );
 	
 	spst.setTime(1,time);
-	spst.setInt(2,rs.getInt(1));
+	spst.setInt(2,t);
 	spst.executeUpdate();
+	
 	con.close();
 	
 	
